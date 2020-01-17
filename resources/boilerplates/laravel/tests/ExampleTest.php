7b94261769a1493fede4db31d8701960e511ba82
Marcel Pociot <m.pociot@gmail.com>
2020-01-17 22:16:38 +0100

    Update ExampleTest.php

--- resources/boilerplates/laravel/tests/ExampleTest.php
+++ resources/boilerplates/laravel/tests/ExampleTest.php
@@ -2,10 +2,17 @@
 
 namespace :vendor_namespace\Tests;
 
-use PHPUnit\Framework\TestCase;
+use Orchestra\Testbench\TestCase;
+use :vendor_namespace\:studly_package_nameServiceProvider;
 
 class ExampleTest extends TestCase
 {
+
+    protected function getPackageProviders($app)
+    {
+        return [:studly_package_nameServiceProvider::class];
+    }
+    
     /** @test */
     public function true_is_true()
     {
